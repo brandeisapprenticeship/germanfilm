ceeacc1a98a9975fe808c34b31e1f30ae9817b78
Amy <ashein@brandeis.edu>
2015-11-09 10:46:57 -0500

    beginnings of submissions

--- gf/config/routes.rb
+++ gf/config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   # root 'welcome#index'
 
   root 'assignments#index'
+
+  root 'submissions#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
