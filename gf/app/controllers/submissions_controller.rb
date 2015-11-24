64237d4c58e91b110a08f0cbb920e8bc74875730
Amy <ashein@brandeis.edu>
2015-11-24 11:09:32 -0500

    More fixed submissions

--- gf/app/controllers/submissions_controller.rb
+++ gf/app/controllers/submissions_controller.rb
@@ -69,6 +69,7 @@ class SubmissionsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def submission_params
-       params.require(:submission).permit(:title, :author, :notecard, :vocabulary)
+      # add author back!!!!
+       params.require(:submission).permit(:title, :notecard, :vocabulary)
     end
 end
