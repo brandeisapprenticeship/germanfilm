8b5fae5246b0a4077acc20e8d101fdb7d4381364
elal22 <elalsh22@brandeis.edu>
2015-12-20 17:32:58 +0000

    bookmark display done

--- gf/app/controllers/submissions_controller.rb
+++ gf/app/controllers/submissions_controller.rb
@@ -47,7 +47,7 @@ class SubmissionsController < ApplicationController
   def update
     respond_to do |format|
       if @submission.update(submission_params)
-        format.html { redirect_to :back, notice: 'Submission was successfully updated.' }
+        format.html { redirect_to submissions_path(:assignment_id => submission_params[:assignment_id]), notice: 'Submission was successfully updated.' }
         format.json { render :show, status: :ok, location: @submission }
       else
         format.html { render :edit }
